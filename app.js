1a8685f6ff6bb83f65821235b7efe14a6e12b832
Safeer Ahmed <19165518a@gmail.com>
2023-07-24 22:41:46 -0400

    added base pi chart

--- app.js
+++ app.js
@@ -70,6 +70,8 @@ function update_chart() {
         create_chart_1(data);
     } else if (curr_chart_num == 2) {
         create_chart_2(data);
+    } else if (curr_chart_num == 3) {
+        create_chart_3(data);
     }
 }
 
@@ -77,8 +79,8 @@ async function init() {
     // create basic main annotations per chart
     const global_annotations = new Map();
     global_annotations.set(1, "The following data is on a random sample of 10,000 hospital patients from across the United States of America in 2019.");
-    global_annotations.set(2, "The chart below highlights the race distribution among the hospital patients throughout 2019.");
-    global_annotations.set(3, "The total distribution of patient sex.");
+    global_annotations.set(2, "The total distribution of patient sex.");
+    global_annotations.set(3, "The chart below highlights the race distribution among the hospital patients throughout 2019.");
     global_annotations.set(4, "Now that we know the patient demographics, begin analyzing patients with stroke appointments.");
 
     setGlobalStatus("global_annotations", global_annotations);
--- chart3.js
+++ chart3.js
@@ -1,1 +1,420 @@
-// this will be a bar chart of male vs females with https://jsfiddle.net/amp42fjn/ as a reference
+function create_chart_3(data) {
+    // Step 0: Calculate the percentage of race distrution based on month
+
+    /**
+     * Race Keys:
+     * 1 = white
+     * 2 = black
+     * 3 = hispanic
+     * 4 = asian or pacific islander
+     * 5 = native american
+     * 6 = other
+     * 
+     * Admission Month:
+     * 1-12 = jan-dec
+     */
+
+    const distribution = new Map(); //key: {month, race} | value: count
+    for (var m = 1; m <= 12; m++) {
+        for (var r = 1; r <= 6; r++) {
+            var key = JSON.stringify({ month: m, race: r });
+            distribution.set(key, 0);
+        }
+    }
+    data.forEach(patient => {
+        const aptMonth = parseInt(patient.AMONTH);
+        const race = parseInt(patient.RACE);
+        if (isNaN(aptMonth) || isNaN(race)) {
+            return;
+        }
+        var key = JSON.stringify({ month: aptMonth, race: race });
+        distribution.set(key, (distribution.get(key) ?? 0) + 1);
+    });
+    console.log(distribution);
+
+    // calculate the total patients per month
+    const totalPatientsPerMonth = new Map();
+    // and calculate the percentage of patients for each race for each month
+    const percentageData = []; // grouped data for percentage of each x value
+
+    for (var m = 1; m <= 12; m++) {
+        var currMonthTotal = 0;
+        for (var r = 1; r <= 6; r++) {
+            var key = JSON.stringify({ month: m, race: r });
+            currMonthTotal += distribution.get(key);
+        }
+        totalPatientsPerMonth.set(m, currMonthTotal);
+
+        for (var r = 1; r <= 6; r++) {
+            var key = JSON.stringify({ month: m, race: r });
+            var cnt = distribution.get(key);
+            var percentage = (cnt / currMonthTotal);
+            percentageData.push({ month: m, race: r, percentage: percentage });
+        }
+    }
+    console.log(totalPatientsPerMonth);
+    console.log(percentageData);
+
+    // Step 1: D3 Line Chart Setup
+    var container = d3.select("#chart-container");
+    container
+        .attr("width", "100%")
+        .attr("height", "100%"); // 100% of body size available
+
+    var containerWidth = parseInt(container.style("width"));
+    var containerHeight = parseInt(container.style("height"));
+
+    // maintain aspect ratio
+    container.attr("viewBox", `0 0 ${containerWidth} ${containerHeight}`)
+        .attr("preserveAspectRatio", "xMinYMin");
+
+    var svg = d3.select("#chart-container"),
+    margin = { top: (0.1 * containerHeight), right: (0.1 * containerWidth), 
+                bottom: (0.1 * containerHeight), left: (0.1 * containerWidth) },
+    width = containerWidth - margin.left - margin.right,
+    height = containerHeight - margin.top - margin.bottom;
+    svg.attr("pointer-events", "none");
+
+    //draw space
+    var g = svg.append("g")
+        .attr("transform", `translate(${margin.left},${margin.top})`);
+
+    //x and y scales
+    var xScale = d3.scaleLinear()
+        .domain([1, 12])
+        .range([0, width]);//.domain(percentageData.map(d => d.month))
+
+    var yScale = d3.scaleLinear()
+        .domain([0, 1])
+        .range([height, 0]);
+
+    //append axes
+    var xTickLabels = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    var formatPercent = d3.format(".0%");
+
+    const xAxis = d3.axisBottom(xScale)
+        .tickFormat((d, i) => xTickLabels[i]);
+
+    const yAxis = d3.axisLeft(yScale)
+        .tickFormat(formatPercent);
+
+    g.append("g")
+        .classed("tick", true)
+        .attr("transform", `translate(0, ${height})`)
+        .call(xAxis);
+
+    g.append("g")
+        .call(yAxis);
+
+    //axes labels
+    svg.append("text")
+        .attr("class", "axis-label")
+        .attr("x", width / 2 + margin.left)
+        .attr("y", height + margin.top + 40)
+        .attr("text-anchor", "middle")
+        .text("Month");
+    
+    svg.append("text")
+        .attr("class", "axis-label")
+        .attr("transform", "rotate(-90)")
+        .attr("x", (-height / 2) - margin.top)
+        .attr("y", margin.left - 40)
+        .attr("text-anchor", "middle")
+        .text("Percentage");
+
+    //colors
+    var colorScale = d3
+        .scaleOrdinal()
+        .domain(percentageData.map(d => d.race))
+        .range(d3.schemeCategory10);
+
+    //last step: line chart -- go one-by-one (use .stack() in future after preprocessing)
+    const raceData1 = percentageData.filter(d => d.race === 1); // returns all entries with race = n
+    const raceData2 = percentageData.filter(d => d.race === 2); // ...
+    const raceData3 = percentageData.filter(d => d.race === 3);
+    const raceData4 = percentageData.filter(d => d.race === 4);
+    const raceData5 = percentageData.filter(d => d.race === 5);
+    const raceData6 = percentageData.filter(d => d.race === 6);
+
+    var areaGeneratorBegin1 = d3
+        .area()
+        .x(0)
+        .y0(yScale(0))
+        .y1(yScale(raceData1[0].percentage));
+
+    var areaGeneratorBegin2 = d3
+        .area()
+        .x(0)
+        .y0(yScale(raceData1[0].percentage))
+        .y1(yScale(raceData1[0].percentage + raceData2[0].percentage));
+
+    var areaGeneratorBegin3 = d3
+        .area()
+        .x(0)
+        .y0(yScale(raceData1[0].percentage + raceData2[0].percentage))
+        .y1(yScale(raceData1[0].percentage + raceData2[0].percentage + raceData3[0].percentage));
+
+    var areaGeneratorBegin4 = d3
+        .area()
+        .x(0)
+        .y0(yScale(raceData1[0].percentage + raceData2[0].percentage + raceData3[0].percentage))
+        .y1(yScale(raceData1[0].percentage + raceData2[0].percentage + raceData3[0].percentage + raceData4[0].percentage));
+
+    var areaGeneratorBegin5 = d3
+        .area()
+        .x(0)
+        .y0(yScale(raceData1[0].percentage + raceData2[0].percentage + raceData3[0].percentage + raceData4[0].percentage))
+        .y1(yScale(raceData1[0].percentage + raceData2[0].percentage + raceData3[0].percentage + raceData4[0].percentage + raceData5[0].percentage));
+
+    var areaGeneratorBegin6 = d3
+        .area()
+        .x(0)
+        .y0(yScale(raceData1[0].percentage + raceData2[0].percentage + raceData3[0].percentage + raceData4[0].percentage + raceData5[0].percentage))
+        .y1(yScale(raceData1[0].percentage + raceData2[0].percentage + raceData3[0].percentage + raceData4[0].percentage + raceData5[0].percentage + raceData6[0].percentage));
+
+    var areaGenerator1 = d3
+        .area()
+        .x(d => xScale(d.month))
+        .y0(yScale(0))
+        .y1(d => yScale(d.percentage));
+
+    var areaGenerator2 = d3
+        .area()
+        .x(d => xScale(d.month))
+        .y0((d, i) => {
+            return yScale(raceData1[i].percentage);
+        })
+        .y1((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage);
+        });
+    
+    var areaGenerator3 = d3
+        .area()
+        .x(d => xScale(d.month))
+        .y0((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage);
+        })
+        .y1((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage + raceData3[i].percentage);
+        });
+
+    var areaGenerator4 = d3
+        .area()
+        .x(d => xScale(d.month))
+        .y0((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage + raceData3[i].percentage);
+        })
+        .y1((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage + raceData3[i].percentage + raceData4[i].percentage);
+        });
+        
+    var areaGenerator5 = d3
+        .area()
+        .x(d => xScale(d.month))
+        .y0((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage + raceData3[i].percentage + raceData4[i].percentage);
+        })
+        .y1((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage + raceData3[i].percentage + raceData4[i].percentage + raceData5[i].percentage);
+        });
+
+    var areaGenerator6 = d3
+        .area()
+        .x(d => xScale(d.month))
+        .y0((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage + raceData3[i].percentage + raceData4[i].percentage + raceData5[i].percentage);
+        })
+        .y1((d, i) => {
+            return yScale(raceData1[i].percentage + raceData2[i].percentage + raceData3[i].percentage + raceData4[i].percentage + raceData5[i].percentage + raceData6[i].percentage);
+        });
+
+    var animDuration = 1000;
+    //race = 1
+    g.append("path")
+        .datum(raceData1)
+        .attr("fill", colorScale(1))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGeneratorBegin1)
+        .attr("stroke", colorScale(1))
+        .attr("stroke-width", 2)
+        .transition()
+        .duration(animDuration)
+        .attr("fill", colorScale(1))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGenerator1)
+        .attr("stroke", colorScale(1))
+        .attr("stroke-width", 2)
+        .on("end", function() {
+            svg.attr("pointer-events", "all");
+        });
+    
+    //race = 2
+    g.append("path")
+        .datum(raceData2)
+        .attr("fill", colorScale(2))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGeneratorBegin2)
+        .attr("stroke", colorScale(2))
+        .attr("stroke-width", 2)
+        .transition()
+        .duration(animDuration)
+        .attr("fill", colorScale(2))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGenerator2)
+        .attr("stroke", colorScale(2))
+        .attr("stroke-width", 2);
+
+    //race = 3
+    g.append("path")
+        .datum(raceData3)
+        .attr("fill", colorScale(3))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGeneratorBegin3)
+        .attr("stroke", colorScale(3))
+        .attr("stroke-width", 2)
+        .transition()
+        .duration(animDuration)
+        .attr("fill", colorScale(3))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGenerator3)
+        .attr("stroke", colorScale(3))
+        .attr("stroke-width", 2);
+
+    //race = 4
+    g.append("path")
+        .datum(raceData4)
+        .attr("fill", colorScale(4))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGeneratorBegin4)
+        .attr("stroke", colorScale(4))
+        .attr("stroke-width", 2)
+        .transition()
+        .duration(animDuration)
+        .attr("fill", colorScale(4))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGenerator4)
+        .attr("stroke", colorScale(4))
+        .attr("stroke-width", 2);
+
+    //race = 5
+    g.append("path")
+        .datum(raceData5)
+        .attr("fill", colorScale(5))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGeneratorBegin5)
+        .attr("stroke", colorScale(5))
+        .attr("stroke-width", 2)
+        .transition()
+        .duration(animDuration)
+        .attr("fill", colorScale(5))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGenerator5)
+        .attr("stroke", colorScale(5))
+        .attr("stroke-width", 2);
+
+    //race = 6
+    g.append("path")
+        .datum(raceData6)
+        .attr("fill", colorScale(6))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGeneratorBegin6)
+        .attr("stroke", colorScale(6))
+        .attr("stroke-width", 2)
+        .transition()
+        .duration(animDuration)
+        .attr("fill", colorScale(6))
+        .attr("fill-opacity", 0.3)
+        .attr("d", areaGenerator6)
+        .attr("stroke", colorScale(6))
+        .attr("stroke-width", 2);
+
+    // adding hovering tool-tips for each area
+
+    // tool tip
+    // inspired by: https://dataviz.unhcr.org/tools/d3/d3_stacked_column_100perc_chart.html | UNHCR
+    var tooltip = d3.select("body")
+        .append("div")
+        .style("display", "inline")
+        .style("position", "fixed")
+        .style("z-index", "10")
+        .style("visibility", "hidden")
+        .text("a simple tooltip");
+
+    g.selectAll("path")
+        .on("mouseover", function(event, d) {
+            console.log("hello world");
+            tooltip
+            .style("visibility", "visible")
+            .style("top", (event.pageY - 28) + "px")
+            .style("left", (event.pageX) + "px");
+
+            d3.select(this)
+                .transition()
+                .duration(200)
+                .attr("opacity", "100%")
+                .attr("filter", "saturate(200%");
+
+            d3.selectAll("path")
+                .filter((d_, i_) => {
+                    if (d_ !== null && d !== null) {
+                        if (d_[0].race !== d[0].race) {
+                            return true;
+                        }
+                        return false;
+                    }
+                })
+                .transition()
+                .duration(200)
+                .attr("opacity", "10%");
+        })
+        .on("mousemove", function(event, d) {
+            tooltip
+                .style("top", (event.pageY - 28) + "px")
+                .style("left", (event.pageX) + "px");
+        })
+        .on("mouseout", function(event, d) {
+            d3.select(this)
+                .transition()
+                .duration(200)
+                .attr("opacity", "100%")
+                .attr("filter", "saturate(100%");
+
+            d3.selectAll("path")
+                .filter((d_, i_) => {
+                    if (d_ !== null && d !== null) {
+                        if (d_[0].race !== d[0].race) {
+                            return true;
+                        }
+                        return false;
+                    }
+                })
+                .transition()
+                .duration(200)
+                .attr("filter", "saturate(100%")
+                .attr("opacity", "100%");
+        })
+        .on("mouseleave", function(event, d) {
+            tooltip.style("visibility", "hidden");
+        });
+
+    // set legend (work in progress)
+    svg.append("rect")
+        .attr("width", 50) //adjust later
+        .attr("height", 50)
+        .style("fill", "#18375F")
+        .attr("transform", `translate(${margin.left + width - 100}, ${margin.top + height - 100})`);
+
+    /**
+     * @TODO
+     * Chart 1: add annotations highlighting the majority of patients as 0 and 90
+     * Chart 2: finish the legend, add race and another statistic to the tooltip, add annotation highlighting lack of purple
+     * Chart 3: create and finish in one sitting
+     * 
+     * Chart 4: scatter plot or some cool other plot from d3 gallery to showcase age and stroke (/whatever disease to easily highlight)
+     *              -include race as color (maintain consistency!)
+     * Chart 5: scatter plot or some cool other plot from d3 gallery to showcase death rate
+     * Chart 6: (if time permits)
+     * 
+     * Essay: verify definition understanding and fully explain all details/features of the project
+     */
+}
